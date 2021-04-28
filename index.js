a7adb2fe42f55abff1fbeb3d7c7d6fe56150ee08
Franklin Yin <yin4thewin@gmail.com>
2021-04-27 21:48:04 -0400

    Allow cors

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ app.get('/:query', async(req, res) => {
 		console.log(error);
 	}
     //Send completed array as response
+    res.set('Access-Control-Allow-Origin', '*')
     res.send(resourcesArray)
 })
 
